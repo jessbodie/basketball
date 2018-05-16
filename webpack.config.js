41f8e97e9c3a642d5d9ca5fbe27c4f94d241f763
Jess <jessbodie@gmail.com>
2018-05-15 22:55:35 -0400

    Config setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/main.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist', {} ),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            favicon: 'src/favicon/favicon.ico'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/style.css',
+        })
+    ],
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.xml$|\.webmanifest$/,
+                loader: 'file-loader?name=favicon/[name].[ext]'  
+            }
+        ]
+    }
+};
